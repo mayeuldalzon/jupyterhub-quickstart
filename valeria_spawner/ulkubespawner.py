3579232fe7797a6ad32e19113b39421678228438
Unknown <guillaume.moutier@gmail.com>
2019-04-15 09:53:43 -0400

    Change image list

--- valeria_spawner/ulkubespawner.py
+++ valeria_spawner/ulkubespawner.py
@@ -5,11 +5,10 @@ class ULKubeSpawner(KubeSpawner):
     def _options_form_default(self):
         with open('/opt/app-root/src/templates/select.html') as file_:
             template = Template(file_.read())
-        image_list = ['s2i-minimal-notebook-s3:3.5', 
-                      's2i-scipy-notebook-s3:3.5', 
-                      's2i-tensorflow-notebook-s3:3.5', 
-                      's2i-minimal-notebook:3.5',
-                      's2i-scipy-notebook:3.6']
+        image_list = ['s2i-minimal-notebook-s3:3.6', 
+                      's2i-scipy-notebook-s3:3.6', 
+                      's2i-tensorflow-notebook-s3:3.6', 
+                      's2i-minimal-notebook:3.6']
         return template.render(image_list=image_list)
 
     def options_from_form(self, formdata):
