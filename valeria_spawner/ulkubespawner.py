913dd87710c816e07c8536002f2ad07abd58fa47
mayeuldalzon <37445841+mayeuldalzon@users.noreply.github.com>
2019-05-31 16:05:02 -0400

    Update ulkubespawner.py

--- valeria_spawner/ulkubespawner.py
+++ valeria_spawner/ulkubespawner.py
@@ -14,7 +14,7 @@ class ULKubeSpawner(KubeSpawner):
                      's2i-spark-minimal-notebook:3.6',
                      's2i-spark-scipy-notebook:3.6',
                      's2i-spark-notebook-s3:3.6',
-                     'r']
+                     'r:3.4.3']
         return template.render(image_list=image_list)
 
     def options_from_form(self, formdata):
